f6d8bbb480aff80f0d7b6d04d8de7d4236122f9b
luiz.coelho <uizhenrique@gmail.com>
2024-11-30 12:25:28 -0400

    Corrigido teste, removendo toString custom e usado de acordo com o gerado pelo Lombok

--- src/test/java/io/t3w/desafio/data/entity/PessoaTest.java
+++ src/test/java/io/t3w/desafio/data/entity/PessoaTest.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.Test;
 
 class PessoaTest {
 
-    @Test
-    public void testToString(){
-        Pessoa pessoa = new Pessoa(666, "0123456789", "Pessoa Teste");
-
-        Assertions.assertEquals("Pessoa{id=666, cpf='0123456789', nome='Pessoa Teste'}", pessoa.toString());
-    }    }
-
+	@Test
+	public void testToString() {
+		Pessoa pessoa = new Pessoa(666, "0123456789", "Pessoa Teste");
+		String esperado = "Pessoa(id=666, cpf=0123456789, nome=Pessoa Teste)";
+		Assertions.assertEquals(esperado, pessoa.toString());
+	}
+}
