e9d14043d151c5e970bf869f9f900a27e78fd56b
luiz.coelho <uizhenrique@gmail.com>
2024-11-30 11:39:27 -0400

    Implementadas operações CRUD para PessoaDAO

--- src/main/java/io/t3w/desafio/data/dao/PessoaDAO.java
+++ src/main/java/io/t3w/desafio/data/dao/PessoaDAO.java
@@ -22,22 +22,6 @@ public class PessoaDAO extends DAO {
         super(connection);
     }
 
-    public List<Pessoa> findAll() throws SQLException {
-        try (PreparedStatement psmt = getConnection().prepareStatement("SELECT * FROM pessoa")) {
-            try (ResultSet rs = psmt.executeQuery()) {
-                final List<Pessoa> pessoas = new ArrayList<>();
-                while (rs.next()) {
-                    final var pessoa = new Pessoa()
-                        .setId(rs.getLong("id"))
-                        .setNome(rs.getString("nome"))
-                        .setCpf(rs.getString("cpf"));
-                    pessoas.add(pessoa);
-                }
-                return pessoas;
-            }
-        }
-    }
-    
     public Pessoa insert(Pessoa pessoa) throws SQLException {
         String sql = "INSERT INTO pessoa (nome, cpf) VALUES (?, ?) RETURNING id";
 
@@ -47,13 +31,68 @@ public class PessoaDAO extends DAO {
 
             try (ResultSet rs = psmt.executeQuery()) {
                 if (rs.next()) {
-                	// Define o ID retornado na instância da pessoa
-                    pessoa.setId(rs.getLong("id")); 
+                    pessoa.setId(rs.getLong("id"));
                 }
             }
         }
 
-        return pessoa; 
+        return pessoa;
     }
 
+    public List<Pessoa> findAll() throws SQLException {
+        String sql = "SELECT * FROM pessoa";
+
+        try (PreparedStatement psmt = getConnection().prepareStatement(sql)) {
+            try (ResultSet rs = psmt.executeQuery()) {
+                List<Pessoa> pessoas = new ArrayList<>();
+                while (rs.next()) {
+                    pessoas.add(parse(rs));
+                }
+                return pessoas;
+            }
+        }
+    }
+
+    public Pessoa findById(long id) throws SQLException {
+        String sql = "SELECT * FROM pessoa WHERE id = ?";
+
+        try (PreparedStatement psmt = getConnection().prepareStatement(sql)) {
+            psmt.setLong(1, id);
+
+            try (ResultSet rs = psmt.executeQuery()) {
+                if (rs.next()) {
+                    return parse(rs);
+                }
+                return null; 
+            }
+        }
+    }
+
+    public Pessoa update(Pessoa pessoa) throws SQLException {
+        String sql = "UPDATE pessoa SET nome = ?, cpf = ? WHERE id = ?";
+
+        try (PreparedStatement psmt = getConnection().prepareStatement(sql)) {
+            psmt.setString(1, pessoa.getNome());
+            psmt.setString(2, pessoa.getCpf());
+            psmt.setLong(3, pessoa.getId());
+
+            int rowsAffected = psmt.executeUpdate();
+            if (rowsAffected == 0) {
+                throw new SQLException("Falha ao atualizar: Pessoa não encontrada.");
+            }
+        }
+
+        return pessoa;
+    }
+
+    public boolean deleteById(long id) throws SQLException {
+        String sql = "DELETE FROM pessoa WHERE id = ?";
+
+        try (PreparedStatement psmt = getConnection().prepareStatement(sql)) {
+            psmt.setLong(1, id);
+
+            int rowsAffected = psmt.executeUpdate();
+            return rowsAffected > 0;
+        }
+    }
 }
