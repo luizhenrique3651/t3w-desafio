c4ab48e6f9771291a1b898312e6bc0e1351b510e
luiz.coelho <uizhenrique@gmail.com>
2024-11-30 12:04:45 -0400

    Adicionado Lombok ao projeto

--- src/main/java/io/t3w/desafio/data/repository/PessoaRepository.java
+++ src/main/java/io/t3w/desafio/data/repository/PessoaRepository.java
@@ -1,9 +1,10 @@
 package io.t3w.desafio.data.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import io.t3w.desafio.data.entity.Pessoa;
-
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 }
