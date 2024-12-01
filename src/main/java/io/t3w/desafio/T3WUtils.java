81e25b71dcb978400a266985a874f53cd417a9b1
luiz.coelho <uizhenrique@gmail.com>
2024-12-01 17:38:06 -0400

    Formatador de valor adicionado ao T3WUtils

--- src/main/java/io/t3w/desafio/T3WUtils.java
+++ src/main/java/io/t3w/desafio/T3WUtils.java
@@ -1,6 +1,8 @@
 package io.t3w.desafio;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public abstract class T3WUtils {
 	public static BigDecimal calcularValorTotal(final int quantidade, final BigDecimal valorUnitario) {
@@ -8,5 +10,9 @@ public abstract class T3WUtils {
 			throw new IllegalArgumentException("Quantidade ou valor unitário inválido");
 		}
 		return valorUnitario.multiply(BigDecimal.valueOf(quantidade));
+	}
+	public static String formataValor(BigDecimal valor) {
+	    NumberFormat formatador = NumberFormat.getCurrencyInstance(Locale.of("pt", "BR"));
+	    return formatador.format(valor);
 	}
 }
