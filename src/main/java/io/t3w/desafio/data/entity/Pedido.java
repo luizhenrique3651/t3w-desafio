2dec30deea32699f0ab8aa1beaec50692ec353e4
luiz.coelho <uizhenrique@gmail.com>
2024-12-01 23:51:48 -0400

    Corrigido problema de recursão infinita no equals & hashCode de Pedido e PedidoItem com Lombok

--- src/main/java/io/t3w/desafio/data/entity/Pedido.java
+++ src/main/java/io/t3w/desafio/data/entity/Pedido.java
@@ -1,11 +1,20 @@
 package io.t3w.desafio.data.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
--- src/main/java/io/t3w/desafio/data/entity/PedidoItem.java
+++ src/main/java/io/t3w/desafio/data/entity/PedidoItem.java
@@ -1,22 +1,33 @@
 package io.t3w.desafio.data.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 public class PedidoItem {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @EqualsAndHashCode.Include
     @JoinColumn(name = "produto_id")
     private Produto produto;
 
+    @EqualsAndHashCode.Include
     private int quantidade;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
